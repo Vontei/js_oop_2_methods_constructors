ddb3a3c4215e123b3368f16c5281cae5c396aaa7
Nick Mattei <nmattei89@gmail.com>
2015-07-13 21:01:27 -0600

    Taxi

--- taxi.js
+++ taxi.js
@@ -1,12 +1,37 @@
 function Taxi(driverName, maxNumberOfPassengers) {
-  // your code here
+  this.driverName = 'Phil Collins';
+  this.maxNumberOfPassengers = maxNumberOfPassengers;
+  this.passengers = []
+
 }
 
 Taxi.prototype.addPassenger = function(passengerName) {
-  // your code here
+  this.passengerName = passengerName
+  this.passengers.push(this.passengerName)
 };
 
-// your code here
+Taxi.prototype.passengerCount = function(passengerName){
+  if(this.passengers == null){
+    return 0
+  } else {
+      return this.passengers.length
+  }
+};
+
+Taxi.prototype.full = function(){
+  if(this.passengerCount == (this.maxNumberOfPassengers -1)){
+    return true;
+  }
+  if(this.passengerCount < this.maxNumberOfPassengers){
+    return false;
+  }
+}
+
+
+Taxi.prototype.dropOffPassengers = function(){
+  this.passengers = []
+  return this.passengers
+}
 
 // DO NOT MODIFY BELOW THIS COMMENT:
 module.exports = Taxi;
